edbe04a0d05e145b756002295910ba6a42b20237
iamssh <saadhasan620@gmail.com>
2020-09-18 11:38:58 +0530

    Complete

--- JS/main.js
+++ JS/main.js
@@ -3,12 +3,17 @@ let textDark = document.getElementsByClassName("light-theme__text-dark");
 let headerBg = document.getElementsByClassName("light-theme__header");
 let cardBg = document.getElementsByClassName("light-theme__card");
 let overviewTitle = document.getElementById("overview");
+let toggleText = document.getElementsByClassName("toggle-text__p")[0];
 let bg = document.getElementsByTagName("body")[0];
-
-let toggler = document.getElementsByClassName("toggle-text")[0];
+let switchEle = document.getElementsByClassName("switch")[0];
+let toggler = document.getElementsByClassName("slider")[0];
 
 toggler.addEventListener("click", () => {
   bg.classList.toggle("dark-theme__bg");
+
+  switchEle.classList.toggle("black_switch");
+
+  toggleText.classList.toggle("dark-theme__text-light");
 
   for (const ele of headerBg) {
     ele.classList.toggle("dark-theme__header");
